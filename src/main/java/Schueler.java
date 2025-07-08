e6ca6cf6ea98972425bf187decacba9011955bef
lukas-bszw <kreitinl@bswiesaude.onmicrosoft.com>
2025-07-08 12:28:56 +0200

    Schüler GUI

--- src/main/java/Schueler.java
+++ src/main/java/Schueler.java
@@ -2,6 +2,10 @@ import javax.swing.*;
 
 public class Schueler extends JDialog {
     private JPanel panel;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JComboBox comboBox1;
+    private JButton speichernButton;
 
     public Schueler() {
         setTitle("Schueler");
